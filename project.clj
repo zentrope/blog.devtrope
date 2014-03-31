21f691070c3768a5d4411c2788438c8e02d5a41d
Keith Irwin <keith@zentrope.com>
2014-03-30 21:00:08 -0700

    Lib update.

--- project.clj
+++ project.clj
@@ -7,10 +7,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.pegdown/pegdown "1.4.2"]
-                 [ch.qos.logback/logback-classic "1.0.13"]
-                 [ring/ring-jetty-adapter "1.2.1"]
+                 [ch.qos.logback/logback-classic "1.1.1"]
+                 [ring/ring-jetty-adapter "1.2.2"]
                  [digest "1.4.3"]]
 
   :jvm-opts ["-Dapple.awt.UIElement=true"]
